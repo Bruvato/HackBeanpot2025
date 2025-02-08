a68983720a630ebbb80785655a0b93e00ed97afa
tylerdong878 <tylerdong878@gmail.com>
2025-02-08 14:08:25 -0500

    spotify bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,24 @@ import Footer from "./components/footer";
 import Hero from "./components/hero";
 import FeaturedDestinations from "./components/featured-destinations";
 import PlaylistGenerator from "./components/playlist-generator";
+import AuthProvider from "./components/providers/auth-provider";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <div className="py-12 bg-gray-50">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <PlaylistGenerator />
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Hero />
+          <div className="py-12 bg-gray-50">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <PlaylistGenerator />
+            </div>
           </div>
-        </div>
-        <FeaturedDestinations />
-      </main>
-      <Footer />
-    </div>
+          <FeaturedDestinations />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
